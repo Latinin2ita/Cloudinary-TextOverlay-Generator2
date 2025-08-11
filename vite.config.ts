301f6e85fb4dc2ff0d93bc8975d936568c503133
Latinin2ita <latinin2Ita@gmail.com>
2025-08-11 18:42:33 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// metti qui il tuo host esatto (quello mostrato nel messaggio “Blocked request…”)
 const RAILWAY_HOST = 'cloudinary-textoverlay-generator2-production.up.railway.app'
 
 export default defineConfig({
